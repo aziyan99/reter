8856e725e0816d771470544c7c7c941c0b323e19
aziyan99 <rajaazian08@gmail.com>
2022-01-15 18:41:31 +0700

    add section to write

--- src/Components/Pages/Write/Write.jsx
+++ src/Components/Pages/Write/Write.jsx
@@ -5,7 +5,16 @@ export default function Write() {
   const [sections, setSections] = useState([]);
   const [newSection, setNewSection] = useState(false);
   const [sectionName, setSectionName] = useState("");
+  const [selectedSection, setSelectedSection] = useState("");
+  const [sectionContent, setSectionContent] = useState([]);
+
   const breadcrumb = [{ name: "Home" }, { name: "Write" }];
+
+  const updateContent = (index, content) => {
+    var newContent = sectionContent;
+    newContent[index] = content;
+    setSectionContent(newContent);
+  };
 
   return (
     <>
@@ -17,7 +26,13 @@ export default function Write() {
           <ul className="list-group">
             {sections.map((section, index) => {
               return (
-                <li className="list-group-item" key={index}>
+                <li
+                  className="list-group-item"
+                  key={index}
+                  onClick={() => {
+                    setSelectedSection(index);
+                  }}
+                >
                   {section}
                 </li>
               );
@@ -36,7 +51,7 @@ export default function Write() {
               </li>
             )}
           </ul>
-          <div className="d-grid">
+          <div className="d-grid gap-2">
             <button
               className="mt-3 btn btn-sm btn-primary"
               onClick={() => {
@@ -50,9 +65,33 @@ export default function Write() {
             >
               {newSection ? "Save" : "New Section"}
             </button>
+            <button className="btn btn-info btn-sm">Download</button>
           </div>
         </div>
-        <div className="col-md-9 mt-2"></div>
+        <div className="col-md-9 mt-2">
+          {selectedSection !== "" && (
+            <>
+              <h1>{sections[selectedSection]}</h1>
+              <textarea
+                className="form-control"
+                rows="12"
+                onChange={(e) => {
+                  updateContent(selectedSection, e.target.value);
+                }}
+              >
+                {sectionContent[selectedSection]}
+              </textarea>
+              <button
+                className="btn btn-sm btn-primary mt-2"
+                onClick={() => {
+                  setSelectedSection("");
+                }}
+              >
+                Save
+              </button>
+            </>
+          )}
+        </div>
       </div>
     </>
   );
