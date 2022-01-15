dd30b92815ca62234e77e02b811510d5f8747c4a
aziyan99 <rajaazian08@gmail.com>
2022-01-15 18:14:36 +0700

    remove home

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,6 +1,11 @@
+import React from "react";
+import Breadcump from "../../Partials/Breadcump";
+
 export default function Home() {
+  const breadcrumb = [{ name: "Home" }];
   return (
     <>
+      <Breadcump items={breadcrumb} />
       <h1>Home</h1>
     </>
   );
