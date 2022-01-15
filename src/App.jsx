4825cbb3270e6cca8425ca0c524a357b303d6e2e
aziyan99 <rajaazian08@gmail.com>
2022-01-15 18:14:03 +0700

    add navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import Navbar from "./Components/Layouts/Navbar";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Pages/Home/Home.jsx";
+import Navbar from "./Components/Layouts/Navbar.jsx";
+import Write from "./Components/Pages/Write/Write.jsx";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-    </>
+      <div className="container mt-3">
+        <Routes>
+          <Route path="/write" element={<Write />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
