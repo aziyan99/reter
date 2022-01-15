693b6294b022bbbb2e15ebf0fb4406eced4c0a77
aziyan99 <rajaazian08@gmail.com>
2022-01-15 18:14:27 +0700

    remove routing to book

--- src/Components/Layouts/Navbar.jsx
+++ src/Components/Layouts/Navbar.jsx
@@ -1,33 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   const href = "#";
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-white border-bottom">
-      <div className="container-fluid">
-        <a className="navbar-brand" href={href}>
-          Navbar
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <a className="nav-link" href={href}>
-              Home
-            </a>
-            <a className="nav-link" href={href}>
-              About
-            </a>
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-white border-bottom p-3">
+        <div className="container">
+          <a className="navbar-brand" href={href}>
+            Navbar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+              <Link className="nav-link" to="/write">
+                Write new book
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
